f0666c3b73c8460cb4da21ff890257a1c983a8df
Vasanth826 <120014385+vasanthkumars26@users.noreply.github.com>
2026-01-21 22:02:13 +0530

    Delete tezt.js

--- tezt.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI, { tls: true })
-  .then(() => console.log("✅ Connected to Atlas"))
-  .catch(err => console.error(err));
